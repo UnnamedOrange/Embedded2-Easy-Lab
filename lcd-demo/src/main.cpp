5da3a841439cdb2c0ae2d7bc360d40bde3a121dd
UnnamedOrange <lycheng1215@sina.com>
2023-10-18 20:28:41 +0800

    Clear screen

--- lcd-demo/src/main.cpp
+++ lcd-demo/src/main.cpp
@@ -1,3 +1,6 @@
+#include <array>
+#include <cstdint>
+
 #include <fpioa.h>
 #include <gpiohs.h>
 #include <sysctl.h>
@@ -5,6 +8,84 @@
 #include "board_config.h"
 #include "lcd.h"
 #include "nt35310.h"
+
+/**
+ * @brief Convert HSV to RGB565.
+ * https://blog.csdn.net/lly_3485390095/article/details/104570885
+ *
+ * @param h From 0 to 360.
+ * @param s From 0 to 100.
+ * @param v From 0 to 100.
+ * @return uint16_t RGB565 to be directly used.
+ */
+static constexpr uint16_t hsv_to_rgb565(int h, int s, int v) {
+    float R = 0, G = 0, B = 0;
+
+    const auto S = float(s) / 100.0F;
+    const auto V = float(v) / 100.0F;
+    if (S == 0) {
+        R = G = B = V;
+    } else {
+        const auto H = float(h) / 60.0F;
+        const auto sector = static_cast<int>(H);
+        const auto remainder = H - sector;
+
+        const auto X = V * (1 - S);
+        const auto Y = V * (1 - S * remainder);
+        const auto Z = V * (1 - S * (1 - remainder));
+        switch (sector) {
+        case 0:
+            R = V;
+            G = Z;
+            B = X;
+            break;
+        case 1:
+            R = Y;
+            G = V;
+            B = X;
+            break;
+        case 2:
+            R = X;
+            G = V;
+            B = Z;
+            break;
+        case 3:
+            R = X;
+            G = Y;
+            B = V;
+            break;
+        case 4:
+            R = Z;
+            G = X;
+            B = V;
+            break;
+        case 5:
+            R = V;
+            G = X;
+            B = Y;
+            break;
+        default:
+            // Unexpected.
+            R = G = B = 0;
+        }
+    }
+    const auto r = static_cast<uint16_t>(R * 255.0F);
+    const auto g = static_cast<uint16_t>(G * 255.0F);
+    const auto b = static_cast<uint16_t>(B * 255.0F);
+    return ((b >> 3) << 0) | ((g >> 2) << 5) | ((r >> 3) << 11);
+}
+struct Colors {
+    std::array<uint16_t, 360> value{};
+    constexpr Colors() {
+        for (size_t i = 0; i < value.size(); i++) {
+            value[i] = hsv_to_rgb565(i, 100, 100);
+        }
+    }
+};
+inline static constexpr Colors _colors;
+inline static const auto& colors = _colors.value;
+
+static uint16_t g_lcd_gram[LCD_X_MAX * LCD_Y_MAX] __attribute__((aligned(128)));
 
 static void io_set_power(void) {
     // Note: 可以设置电源域。
@@ -31,5 +112,10 @@ int main() {
     io_set_power();
     lcd_init();
     lcd_set_direction(DIR_YX_RLDU);
-    lcd_clear(RED);
+
+    while (true) {
+        for (const auto color : colors) {
+            lcd_clear(color);
+        }
+    }
 }
